a8959d99a7b27992fc19078b0be7c762928277e9
alexmejiam10 <jorgemejia189172@correo.itm.edu.co>
2021-02-15 20:11:52 -0500

    Eliminar Tarea
    viedo 16 donde elimina una tarea

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React, {useState} from 'react'
-import { isEmpty } from 'lodash'
+import { isEmpty, size } from 'lodash'
 import shortid from 'shortid'
 
 function App() {
@@ -21,6 +21,10 @@ function App() {
    setTask("")//poner para poder limpiar campo despues de enviar
   } 
 
+  const deleteTask = (id) =>{
+   const filteredTasks = tasks.filter(task =>  task.id !== id)
+   setTasks(filteredTasks)//settasks es el que me trae todas las tareas
+  }
   return (
     <div className="container mt-5">
 
@@ -29,19 +33,34 @@ function App() {
       <div className="row">
         <div className="col-8">
          <h4 className="text-center">Lista de Tareas</h4> 
-         <ul className="list-group">
           {
-            tasks.map((task) => ( // el map nos sirve para iterar
-              <li className="list-group-item" key={task.id}>
-              <span className="lead">{task.name}</span>
-               <button className ="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-               <button className ="btn btn-warning btn-sm float-right">Editar</button>
-             </li>
-             )
+            size(tasks) == 0 ?(
+           <h5 className="text-center">Aun no hay tareas programadas</h5>
+            ):
+            (
+            <ul className="list-group">
+              {
+                // es para crear la lista de tareas
+                tasks.map((task) => ( // el map nos sirve para iterar
+                  <li className="list-group-item" key={task.id}>
+                  <span className="lead">{task.name}</span>
+
+                  <button 
+                    className ="btn btn-danger btn-sm float-right mx-2"
+                    onClick={() => deleteTask(task.id)}//nos sirve para decir que el boton realizo una accion
+                    >
+                    Eliminar
+                  </button>
+
+                  <button className ="btn btn-warning btn-sm float-right">Editar</button>
+                </li>
+                )
+                )
+              
+              }
+            </ul> 
             )
-          
-          }
-         </ul> 
+           }
         </div>
 
         <div className="col-4">
